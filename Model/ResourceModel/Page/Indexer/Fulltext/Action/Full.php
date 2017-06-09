4389a2058b56d69744d842be66c54551067541e2
Romain Ruaud <romain.ruaud@smile.fr>
2017-06-09 14:59:50 +0200

    Ensure indexation is working properly for both 2.1 CE and 2.1 EE

--- Model/ResourceModel/Page/Indexer/Fulltext/Action/Full.php
+++ Model/ResourceModel/Page/Indexer/Fulltext/Action/Full.php
@@ -13,6 +13,10 @@
 
 namespace Smile\ElasticsuiteCms\Model\ResourceModel\Page\Indexer\Fulltext\Action;
 
+use Magento\Cms\Api\Data\PageInterface;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\EntityManager\MetadataPool;
+use Magento\Store\Model\StoreManagerInterface;
 use Smile\ElasticsuiteCore\Model\ResourceModel\Indexer\AbstractIndexer;
 
 /**
@@ -24,6 +28,27 @@ use Smile\ElasticsuiteCore\Model\ResourceModel\Indexer\AbstractIndexer;
  */
 class Full extends AbstractIndexer
 {
+    /**
+     * @var \Magento\Framework\EntityManager\MetadataPool
+     */
+    private $metadataPool;
+
+    /**
+     * Full constructor.
+     *
+     * @param \Magento\Framework\App\ResourceConnection     $resource     Resource Connection
+     * @param \Magento\Store\Model\StoreManagerInterface    $storeManager Store Manager
+     * @param \Magento\Framework\EntityManager\MetadataPool $metadataPool Metadata Pool
+     */
+    public function __construct(
+        ResourceConnection $resource,
+        StoreManagerInterface $storeManager,
+        MetadataPool $metadataPool
+    ) {
+        $this->metadataPool = $metadataPool;
+        parent::__construct($resource, $storeManager);
+    }
+
     /**
      * Load a bulk of cms page data.
      *
@@ -37,7 +62,7 @@ class Full extends AbstractIndexer
     public function getSearchableCmsPage($storeId, $cmsPageIds = null, $fromId = 0, $limit = 100)
     {
         $select = $this->getConnection()->select()
-            ->from(['p' => $this->getTable('cms_page')]);
+                       ->from(['p' => $this->getTable('cms_page')]);
 
         $this->addIsVisibleInStoreFilter($select, $storeId);
 
@@ -46,9 +71,9 @@ class Full extends AbstractIndexer
         }
 
         $select->where('p.page_id > ?', $fromId)
-            ->where('p.is_searchable = ?', true)
-            ->limit($limit)
-            ->order('p.page_id');
+               ->where('p.is_searchable = ?', true)
+               ->limit($limit)
+               ->order('p.page_id');
 
         return $this->connection->fetchAll($select);
 
@@ -64,11 +89,13 @@ class Full extends AbstractIndexer
      */
     private function addIsVisibleInStoreFilter($select, $storeId)
     {
+        $linkField = $this->metadataPool->getMetadata(PageInterface::class)->getLinkField();
+
         $select->join(
             ['ps' => $this->getTable('cms_page_store')],
-            'p.page_id = ps.page_id'
+            "p.$linkField = ps.$linkField"
         );
-        $select->where('ps.store_id IN (?)', array(0, $storeId));
+        $select->where('ps.store_id IN (?)', [0, $storeId]);
 
         return $this;
     }
